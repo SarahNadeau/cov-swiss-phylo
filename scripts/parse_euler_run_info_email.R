59455259099629d78a1557f7853b702d4349008c
Sarah Nadeau <sarah.nadeau@bsse.ethz.ch>
2021-07-06 11:36:24 +0200

    update with current run info

--- scripts/parse_euler_run_info_email.R
+++ scripts/parse_euler_run_info_email.R
@@ -1,10 +1,14 @@
 #' This script is to clean run info copy-pasted from Euler emails.
 
 OUTFILE <- "results_all/run_info.csv"
-run_info <- read.csv(file = "~/Downloads/run_info.csv")
+run_info <- read.csv(file = "results_all/run_info_euler_raw.csv")
 
+run_info_clean <- run_info %>% 
+  mutate(value = gsub(x = value, pattern = "\ *:\ *", replacement = ":")) %>%
+  tidyr::separate(col = value, into = c("metric", "value"), sep = ":")
+  
 run_info_clean <- as.data.frame(apply(
-  X = run_info,
+  X = run_info_clean,
   MARGIN = 2, 
   FUN = trimws,
   whitespace = "[\\h\\v]")) %>%
