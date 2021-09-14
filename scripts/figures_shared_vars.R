a0efa2b455f5baf2209dc68d1fa0b3c14a55b5db
nadeaus <sarah.nadeau@bsse.ethz.ch>
2021-09-14 19:10:26 +0200

    change from decrease to damping

--- scripts/figures_shared_vars.R
+++ scripts/figures_shared_vars.R
@@ -24,7 +24,7 @@ facet_nospace_theme_elements <- theme(
   panel.spacing = unit(0, "lines"))
 border_closure_end <- as.Date("2020-06-15")
 border_closure_start <- as.Date("2020-03-25")
-ct_factor_x_lab <- "Tranmission rate decrease (%)"
+ct_factor_x_lab <- "Tranmission rate damping (%)"
 ct_factor_y_lab <- "Posterior support"
 chains_assumption_legend_name <- "Polytomy assumption"
 chains_assumption_colors <- RColorBrewer::brewer.pal(n = 3, name = "Dark2")[1:2]
